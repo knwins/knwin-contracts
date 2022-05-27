e3e88cf2cadf62f9ef999711d960e7c5f26e7bc6
knwins <knwin@msn.com>
2022-05-27 11:26:37 +0800

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,35 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require('hardhat');
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const FBNFT = await hre.ethers.getContractFactory('FBNFT');
+  // 替换成你的盲盒 ipfs 地址
+  const fbnft = await FBNFT.deploy(
+    'ipfs://QmbyUfWA5fuedutDAJ5CPs4ujVAfhPhn2Hi1URhAPwYJM7/'
+  );
+
+  await fbnft.deployed();
+
+  console.log('Deployed to:', fbnft.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
